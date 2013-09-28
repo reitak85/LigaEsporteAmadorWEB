1b0aef4f6d0c968b0e7c543fcb4a0c40f163cb7f
luizHenri <luizsouzatwo@gmail.com>
2013-09-28 13:18:38 -0300

    atualizacao

---
+++ src/br/com/ligaesporteamador/model/Noticia.java
@@ -0,0 +1,147 @@
+package br.com.ligaesporteamador.model;
+
+import java.util.Calendar;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+
+@Table(name = "NOTICIA_IMAGEM")
+public class Noticia {
+
+	@Id
+	@GeneratedValue
+	@Column(name = "NOTICIA_IMAGEM_ID", length = 10)
+	private Long id;
+
+	@Column(name = "TITULO")
+	private String titulo;
+
+	@Column(name = "SUB_TITULO")
+	private String subTitulo;
+
+	@Column(name = "CREDITO")
+	private String credito;
+
+	@Column(name = "LEGENDA")
+	private String legenda;
+
+	@Temporal(TemporalType.DATE)
+	@Column(name = "DATA_DA_NOTICIA")
+	private Calendar data;
+
+	@Column(name = "IMAGEM")
+	private String imagem;
+
+	@Column(name = "NOTICIA_UM")
+	private String noticiaUm;
+
+	@Column(name = "NOTICA_DOIS")
+	private String noticiaDois;
+
+	@Column(name = "NOTICIA_TRES")
+	private String noticiaTres;
+
+	@ManyToOne
+	@Column(name = "ESPORTE_ID")
+	private Esporte esporte;
+
+	public Noticia() {
+		esporte = new Esporte();
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getTitulo() {
+		return titulo;
+	}
+
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+
+	public String getSubTitulo() {
+		return subTitulo;
+	}
+
+	public void setSubTitulo(String subTitulo) {
+		this.subTitulo = subTitulo;
+	}
+
+	public String getCredito() {
+		return credito;
+	}
+
+	public void setCredito(String credito) {
+		this.credito = credito;
+	}
+
+	public String getLegenda() {
+		return legenda;
+	}
+
+	public void setLegenda(String legenda) {
+		this.legenda = legenda;
+	}
+
+	public Calendar getData() {
+		return data;
+	}
+
+	public void setData(Calendar data) {
+		this.data = data;
+	}
+
+	public String getImagem() {
+		return imagem;
+	}
+
+	public void setImagem(String imagem) {
+		this.imagem = imagem;
+	}
+
+	public String getNoticiaUm() {
+		return noticiaUm;
+	}
+
+	public void setNoticiaUm(String noticiaUm) {
+		this.noticiaUm = noticiaUm;
+	}
+
+	public String getNoticiaDois() {
+		return noticiaDois;
+	}
+
+	public void setNoticiaDois(String noticiaDois) {
+		this.noticiaDois = noticiaDois;
+	}
+
+	public String getNoticiaTres() {
+		return noticiaTres;
+	}
+
+	public void setNoticiaTres(String noticiaTres) {
+		this.noticiaTres = noticiaTres;
+	}
+
+	public Esporte getEsporte() {
+		return esporte;
+	}
+
+	public void setEsporte(Esporte esporte) {
+		this.esporte = esporte;
+	}
+
+}
