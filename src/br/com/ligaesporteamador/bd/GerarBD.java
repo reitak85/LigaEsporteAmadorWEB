6737b64269ca13cba2196a849b4aa1c270bddb64
reitak85 <reinaldo.takahashi@hotmail.com>
2013-12-04 02:15:59 +0100

    Dados default de esportes e categorias

--- src/br/com/ligaesporteamador/bd/GerarBD.java
+++ src/br/com/ligaesporteamador/bd/GerarBD.java
@@ -1,7 +1,10 @@
 package br.com.ligaesporteamador.bd;
 
 
+import java.util.ArrayList;
 import java.util.Calendar;
+import java.util.HashMap;
+import java.util.List;
 
 import org.hibernate.ejb.Ejb3Configuration;
 import org.hibernate.tool.hbm2ddl.SchemaExport;
@@ -9,6 +12,7 @@ import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Controller;
 
 import br.com.ligaesporteamador.model.BaseEntity;
+import br.com.ligaesporteamador.model.CategoriaEsporte;
 import br.com.ligaesporteamador.model.Esporte;
 import br.com.ligaesporteamador.model.UsuarioSistema;
 import br.com.ligaesporteamador.util.MD5;
@@ -28,7 +32,23 @@ public class GerarBD extends DataAccessService<BaseEntity>{
 			
 			recriaSchema();
 			createUser();
-			//createEsporte("Futebol", "Futebol");
+			List<String> listDefault = new ArrayList<String>();
+			listDefault.add("Nenhum");
+			listDefault.add("Masculino");
+			listDefault.add("Feminino");
+			List<String> listCatFut = new ArrayList<String>();
+			listCatFut.add("Nenhum");
+			listCatFut.add("Sub 09");
+			listCatFut.add("Sub 11");
+			listCatFut.add("Sub 13");
+			listCatFut.add("Sub 15");
+			listCatFut.add("Sub 17");
+			createEsporte("Futebol", "Futebol", listCatFut);
+			createEsporte("Basquete", "Basquete", listDefault);
+			createEsporte("Tênis", "Tenis", listDefault);
+			createEsporte("Vôlei", "Vôlei", listDefault);
+			createEsporte("Handbol", "Handbol", listDefault);
+			createEsporte("Rugby", "Rugby", listDefault);
 			
 		} catch (Exception e) {
 			e.printStackTrace();
@@ -45,13 +65,23 @@ public class GerarBD extends DataAccessService<BaseEntity>{
 		create(usuarioSistema);
 	}
 	
-	public void createEsporte(String esportee, String desc) throws Exception{
+	public void createEsporte(String esportee, String desc, List<String> categorias) throws Exception{
 		Esporte esporte = new Esporte();
 		esporte.setNome(esportee);
 		esporte.setDescricao(desc);
 		create(esporte);
+		List<Esporte> listEsportes = (List)createQuery("SELECT e FROM Esporte e where e.nome='"+esportee+"'");
+		esporte = listEsportes.isEmpty() ? null : listEsportes.get(0);
+		System.out.println(esporte);
+		for (String categoria : categorias) {
+			CategoriaEsporte c = new CategoriaEsporte();
+			c.setEsporte(esporte);
+			c.setNome(categoria);
+			c.setDescricao(categoria);
+			create(c);
+		}
 	}
-
+	
 	public void recriaSchema() throws Exception {
 
 		System.out.println("Recriando Schema...");
